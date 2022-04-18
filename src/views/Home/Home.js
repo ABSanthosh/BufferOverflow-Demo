3ee0a6c3222bb0e48c35716b148db5075287025f
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-18 13:20:47 +0530

    feat(Home): Added all tabs to home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,23 +1,48 @@
-import logo from "../../logo.svg";
+import { useState } from "react";
+import StackGuard from "../StackGuard/StackGuard";
 import "./Home.scss";
+import Overflow from "../Overflow/Overflow";
+import StackShield from "../StackShield/StackShield";
 
 export default function Home() {
+  const [tabNo, setTabNo] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/views/Home/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="HomeWrapper">
+      <h1 className="HomeWrapper__Title">Buffer Overflow</h1>
+      <main className="HomeWrapper__Container">
+        <nav className="HomeWrapper__NavBar">
+          <li
+            className={`HomeWrapper__NavBar--Item ${
+              tabNo === 0 ? "HomeWrapper__NavBar--Item--Active" : ""
+            }`}
+            onClick={() => setTabNo(0)}
+          >
+            Overflow
+          </li>
+          <li
+            className={`HomeWrapper__NavBar--Item ${
+              tabNo === 1 ? "HomeWrapper__NavBar--Item--Active" : ""
+            }`}
+            onClick={() => setTabNo(1)}
+          >
+            Stack Guard
+          </li>
+          <li
+            className={`HomeWrapper__NavBar--Item ${
+              tabNo === 2 ? "HomeWrapper__NavBar--Item--Active" : ""
+            }`}
+            onClick={() => setTabNo(2)}
+          >
+            Stack Shield
+          </li>
+        </nav>
+        <section className="HomeWrapper__Section">
+          {tabNo === 0 && <Overflow />}
+          {tabNo === 1 && <StackGuard />}
+          {tabNo === 2 && <StackShield />}
+        </section>
+      </main>
     </div>
   );
 }
