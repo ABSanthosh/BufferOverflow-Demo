ca5929cd6480db06cd92dc77c5788299f4eab4f4
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-18 13:20:27 +0530

    feat(Overflow): New StackShield

---
+++ src/views/StackShield/StackShield.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./StackShield.scss";
+
+function StackShield(props) {
+  return <div className="StackShieldWrapper">Stack Shield</div>;
+}
+
+StackShield.propTypes = {
+  // bla: PropTypes.string,
+};
+
+StackShield.defaultProps = {
+  // bla: 'test',
+};
+
+export default StackShield;
