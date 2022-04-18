3fd7d305b5569d0c32cd19435e1ff74a1cff2817
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-18 13:20:11 +0530

    feat(Overflow): New Stack Guard page

---
+++ src/views/StackGuard/StackGuard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./StackGuard.scss";
+
+function StackGuard(props) {
+  return <div className="StackGuardWrapper">Stack guard</div>;
+}
+
+StackGuard.propTypes = {
+  // bla: PropTypes.string,
+};
+
+StackGuard.defaultProps = {
+  // bla: 'test',
+};
+
+export default StackGuard;
