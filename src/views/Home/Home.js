94a9d1b4d62993e5aff5301be7cf4a5ad2ee46d0
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-19 15:30:43 +0530

    feat(Home): Tab based on router

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,46 +1,49 @@
-import { useState } from "react";
 import StackGuard from "../StackGuard/StackGuard";
 import "./Home.scss";
 import Overflow from "../Overflow/Overflow";
 import StackShield from "../StackShield/StackShield";
+import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
-  const [tabNo, setTabNo] = useState(0);
+  const { tab } = useParams();
 
   return (
     <div className="HomeWrapper">
       <h1 className="HomeWrapper__Title">Buffer Overflow</h1>
       <main className="HomeWrapper__Container">
         <nav className="HomeWrapper__NavBar">
-          <li
+          <Link
             className={`HomeWrapper__NavBar--Item ${
-              tabNo === 0 ? "HomeWrapper__NavBar--Item--Active" : ""
+              parseInt(tab) === 0 ? "HomeWrapper__NavBar--Item--Active" : ""
             }`}
-            onClick={() => setTabNo(0)}
+            to="/0"
+            // onClick={() => setTabNo(0)}
           >
             Overflow
-          </li>
-          <li
+          </Link>
+          <Link
             className={`HomeWrapper__NavBar--Item ${
-              tabNo === 1 ? "HomeWrapper__NavBar--Item--Active" : ""
+              parseInt(tab) === 1 ? "HomeWrapper__NavBar--Item--Active" : ""
             }`}
-            onClick={() => setTabNo(1)}
+            to="/1"
+            // onClick={() => setTabNo(1)}
           >
             Stack Guard
-          </li>
-          <li
+          </Link>
+          <Link
             className={`HomeWrapper__NavBar--Item ${
-              tabNo === 2 ? "HomeWrapper__NavBar--Item--Active" : ""
+              parseInt(tab) === 2 ? "HomeWrapper__NavBar--Item--Active" : ""
             }`}
-            onClick={() => setTabNo(2)}
+            to="/2"
+            // onClick={() => setTabNo(2)}
           >
             Stack Shield
-          </li>
+          </Link>
         </nav>
         <section className="HomeWrapper__Section">
-          {tabNo === 0 && <Overflow />}
-          {tabNo === 1 && <StackGuard />}
-          {tabNo === 2 && <StackShield />}
+          {parseInt(tab) === 0 && <Overflow />}
+          {parseInt(tab) === 1 && <StackGuard />}
+          {parseInt(tab) === 2 && <StackShield />}
         </section>
       </main>
     </div>
