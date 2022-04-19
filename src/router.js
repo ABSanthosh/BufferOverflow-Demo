53ee1b81ee0a48bf8cac7398390368fb95afcd1d
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-19 15:30:21 +0530

    feat(Route):Router based navigation

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import * as views from "./views"
-import React from 'react'
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import * as views from "./views";
+import React from "react";
 
 export default function Router() {
-    return (<BrowserRouter>
+  return (
+    <BrowserRouter>
       <Switch>
-        <Route exact path="/"  component={views.Home} />
+        <Route exact path="/:tab" component={views.Home} />
+        {/* redirect / to /0 */}
+        <Route exact path="/" render={() => <Redirect to="/0" />} />
       </Switch>
     </BrowserRouter>
-    )
+  );
 }
